8630ffa922314936d54388fd6d2da53cbf91fe58
PickleFace5 <caden.dalley@icloud.com>
2022-07-26 09:44:12 -0700

    Looker now flops when not in water

--- src/main/java/io/github/clebian/thalassophobia/entity/client/render/LookerRenderer.java
+++ src/main/java/io/github/clebian/thalassophobia/entity/client/render/LookerRenderer.java
@@ -2,6 +2,7 @@ package io.github.clebian.thalassophobia.entity.client.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Vector3f;
 import io.github.clebian.thalassophobia.Thalassophobia;
 import io.github.clebian.thalassophobia.entity.LookerEntity;
 import io.github.clebian.thalassophobia.entity.client.LookerModel;
@@ -9,6 +10,8 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.animal.Cod;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
 public class LookerRenderer extends GeoEntityRenderer<LookerEntity> {
@@ -28,6 +31,18 @@ public class LookerRenderer extends GeoEntityRenderer<LookerEntity> {
                                     ResourceLocation textureLocation) {
         stack.scale(0.8F, 0.8F, 0.8F);
         return super.getRenderType(animatable, partialTicks, stack, renderTypeBuffer, vertexBuilder, packedLightIn, textureLocation);
+    }
+
+    @Override
+    protected void applyRotations(LookerEntity pEntityLiving, PoseStack pMatrixStack, float pAgeInTicks, float pRotationYaw, float pPartialTicks) {
+        super.applyRotations(pEntityLiving, pMatrixStack, pAgeInTicks, pRotationYaw, pPartialTicks);
+        float f = 4.3F * Mth.sin(0.6F * pAgeInTicks);
+        pMatrixStack.mulPose(Vector3f.YP.rotationDegrees(f));
+        if (!pEntityLiving.isInWater()) {
+            pMatrixStack.translate((double)0.1F, (double)0.1F, (double)-0.1F);
+            pMatrixStack.mulPose(Vector3f.ZP.rotationDegrees(90.0F));
+        }
+
     }
 
 
